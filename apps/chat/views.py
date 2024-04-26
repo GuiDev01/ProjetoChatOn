b66c6fbe155819450ee2d801f9b4da411d459e00
Guilherme Phili <guilhermephilip25@gmail.com>
2024-04-26 15:54:09 -0300

    Atualização

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+from .models import Message, Room
+
+from django.views.generic.detail import DetailView
+
+
+def home(request):
+    rooms = Room.objects.all()
+
+    return render(request,'chat/home.html', { 
+        'rooms': rooms 
+    })
+
+
+class RoomDetailView(DetailView):
+    model = Room
+    template_name = 'chat/list-messages.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
